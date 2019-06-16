1be81aa421789a9d4081b4e02e9891dd4b462320
Zhouchanglong <changlong.zhou@56qq.com>
2019-06-16 17:15:41 +0800

    feat: 添加README文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // 将 ESLint 限制到一个特定的项目，在配置文件里设置 "root": true
+  // ESLint 一旦发现配置文件中有 "root": true，它就会停止在父级目录中寻找
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
@@ -15,6 +17,8 @@ module.exports = {
   rules: {
     'react/prefer-stateless-function': 0,
     'react/jsx-filename-extension': 0,
+    'react/jsx-uses-react': 2,
+    'react/jsx-uses-vars': 2,
     'no-alert': 2,
     semi: 1,
     curly: 2,
@@ -23,8 +27,10 @@ module.exports = {
   },
   plugins: [
     'react',
+    'babel',
   ],
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended'
   ],
   globals: {
